17c6fd302ad67987d5f00e363e270db556bb5e4b
Dipali <dipalikawade08@gmail.com>
2026-09-16 14:55:16 +0530

    Fix CORS

--- Main.py
+++ Main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from Routes.stdroutes import router
 
-app  = FastAPI()
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
 @app.get("/")
 def greet():
-    return {"message":"hello"}
+    return {"message": "hello"}
